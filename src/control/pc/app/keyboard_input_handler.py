a616fde49f49803344ee7583bbae414fcfb184f2
MorganKryze <103436411+MorganKryze@users.noreply.github.com>
2025-02-18 22:43:28 +0100

    🌟 feat: upgraded keyboard input reliability

--- src/control/pc/app/keyboard_input_handler.py
+++ src/control/pc/app/keyboard_input_handler.py
@@ -13,26 +13,37 @@ class KeyboardInputHandler(InputHandler):
         super().__init__(websocket_url)
         logging.info("[KeyboardInputHandler] Initialized.")
         self.listener = keyboard.Listener(on_press=self.on_press)
+        self.key_actions = {
+            keyboard.Key.up: lambda: self.send_position(Position(0, 1)),
+            keyboard.Key.down: lambda: self.send_position(Position(0, -1)),
+            keyboard.Key.right: lambda: self.send_position(Position(-1, 0)),
+            keyboard.Key.left: lambda: self.send_position(Position(1, 0)),
+            keyboard.Key.esc: self.listener.stop,
+        }
 
-    def on_press(self, key):
+    def on_press(self, key: keyboard.Key) -> None:
+        """
+        Handle key press events and send corresponding positions to the server.
+        """
+        action = self.key_actions.get(key)
+        if action:
+            action()
+
+    def handle_input(self) -> None:
+        """
+        Start the keyboard listener and handle input events.
+        """
         try:
-            if key == keyboard.Key.up:
-                self.send_position(Position(0, 1))
-            elif key == keyboard.Key.down:
-                self.send_position(Position(0, -1))
-            elif key == keyboard.Key.right:
-                self.send_position(Position(-1, 0))
-            elif key == keyboard.Key.left:
-                self.send_position(Position(1, 0))
-            elif key == keyboard.Key.esc:
-                self.listener.stop()
-        except AttributeError:
-            pass
+            with self.listener as listener:
+                listener.join()
+        except KeyboardInterrupt:
+            logging.info("[KeyboardInputHandler] Stopping listener.")
+        except Exception as e:
+            logging.error(f"[KeyboardInputHandler] Error: {e}")
 
-    def handle_input(self):
-        self.listener.start()
-        self.listener.join()
-
-    def start(self):
+    def start(self) -> None:
+        """
+        Start the input handler.
+        """
         super().start()
         self.handle_input()
